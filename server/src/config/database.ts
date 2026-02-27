d58964bbb577ba7d6f2d41d9b67183ea0d1f6283
diqpet-oss <diqpet@gmail.com>
2026-02-27 13:30:09 +0800

    Update database.ts

--- server/src/config/database.ts
+++ server/src/config/database.ts
@@ -2,7 +2,8 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () => {
   try {
-    const mongoUri = process.env.MONGODB_URI || 'mongodb+srv://diqpet_db_user:2a26kiMRDEC5b88T@diqpet.fh8nhhv.mongodb.net/?appName=diqpet';
+    // 使用环境变量或默认连接字符串
+    const mongoUri = process.env.MONGODB_URI || 'mongodb+srv://diqpet_db_user:2a26kiMRDEC5b88T@diqpet.fh8nhhv.mongodb.net/diqpet?retryWrites=true&w=majority';
     await mongoose.connect(mongoUri);
     console.log('MongoDB 连接成功');
   } catch (error) {
